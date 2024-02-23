baa8fa27d86105ea26923920a7a8e0c0b2cbd763
seoyun-lee <20212025@o365.hanbat.ac.kr>
2024-02-23 23:12:28 +0900

    chap01 : start typescript

---
+++ chap01/index.ts
@@ -0,0 +1,14 @@
+const hello: string = "world";
+function add(x: number, y: number): number {
+    return x + y;
+}
+
+interface Person {
+    name: string,
+    age: number,
+}
+
+const person: Person = {
+    name: "Choonsik",
+    age: 23,
+};
