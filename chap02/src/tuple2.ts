a37a30e1ab91a3cc4ec961905ee8cd0ec4cfaf1e
seoyun-lee <20212025@o365.hanbat.ac.kr>
2024-03-05 22:33:33 +0900

    chap02 : typescript basic grammar

---
+++ chap02/src/tuple2.ts
@@ -0,0 +1,3 @@
+const tuple2: readonly [number, boolean, string] = [1, false, "hi"];
+// tuple2.push("no");
+// Property 'push' does not exist on type 'readonly [number, boolean, string]'
---
+++ chap02/src/tuple3.ts
@@ -0,0 +1,3 @@
+const array2: [number, number, number] = [123, 4, 56];
+// array2[3].toFixed();
+// Object is possibly 'undefined'
